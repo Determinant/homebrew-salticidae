204cf00b869aa371ab1988e808164626754c78de
Determinant <tederminant@gmail.com>
2020-05-15 17:40:45 -0400

    ...

---
+++ salticidae@31.rb
@@ -0,0 +1,21 @@
+class SalticidaeAT31 < Formula
+  desc "Minimal C++ asynchronous network library for distributed systems"
+  homepage "https://github.com/Determinant/salticidae"
+  url "https://github.com/Determinant/salticidae/archive/v0.3.1.tar.gz"
+  sha256 "f62819106cd06f9d48145355a40811b78cff24a6fff6e45122875a7c50f893ed"
+
+  depends_on "cmake" => :build
+  depends_on "make" => :build
+  depends_on "libuv" => "1.10"
+  depends_on "openssl@1.1" => "1.1"
+
+  def install
+    system "cmake", ".", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_SHARED=ON", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
